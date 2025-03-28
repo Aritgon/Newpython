b512a79ac9ef95ab118370232c4884b84b48d1a7
Aritgon <aritgon1947@gmail.com>
2025-03-28 11:49:40 +0530

    Initial stage

---
+++ new.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# basic numpy.
+array = np.array([1,2,3,4,5])
+# print(array.ndim) # this will print dimensions of the array.
+
+array1 = np.array([[1,2,3], [4,5,6]])
+# print(array1.ndim)
+print(array1[0,1] + array1[1,1]) # sum of 2 elems with slicing.
